67a066e5434cdde6e166b452415fefd62d930d95
huequica <dev@huequica.xyz>
2026-07-12 22:28:17 +0900

    色の変更機能がほしかったので追加

--- src/components/SkillPill.tsx
+++ src/components/SkillPill.tsx
@@ -1,21 +1,29 @@
 import type { FC } from 'react';
 
+interface Colors {
+  text?: string;
+  background?: string;
+}
 interface SkillPillProps {
   skill: string;
+  color?: Colors;
 }
 
-export const SkillPill: FC<SkillPillProps> = ({ skill }) => {
+export const SkillPill: FC<SkillPillProps> = ({
+  skill,
+  color = { text: 'text-violet-300', background: 'bg-violet-500/10' },
+}) => {
   const requirementClasses = [
     'inline-flex',
     'items-center',
     'justify-center',
     'rounded-full',
-    'bg-violet-500/10',
     'px-4',
     'py-2',
     'text-sm',
     'font-semibold',
-    'text-violet-300',
+    color.background,
+    color.text,
   ].join(' ');
   return <span className={requirementClasses}>{skill}</span>;
 };
